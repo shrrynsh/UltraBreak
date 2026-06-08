06e88d356b2176c99ec9d4c4deebb58ef11372e2
Shreyansh Modi <shrynshmodi@gmail.com>
2026-06-08 18:21:59 +0000

    added

--- run_evaluation.sh
+++ run_evaluation.sh
@@ -13,4 +13,4 @@ source $(conda info --base)/etc/profile.d/conda.sh
 conda activate ultrabreak
 
 # Run your python script
-python evaluation/evaluate.py --attack_result results/safebench_jailbroken_mode_5000/THUDM/GLM-4.1V-9B-Thinking.csv
+python evaluation/evaluate.py --attack_result results/safebench_jailbroken_mode_1300/Qwen/Qwen2.5-VL-7B-Instruct.csv
